ed201c51a62c585df80910ba7319a84d97fd4915
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-02-12 21:00:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="traffiLight">
+      <div className="light red"></div>
+      <div className="light yellow"></div>
+      <div className="light green"></div>
+    </div>
+  );
+}
+
+export default App;
