f18105af0d96d7eeffdf867b3a95f0720860612d
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-02-13 17:12:48 -0300

    traffic light working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [light, setLight] = useState(0);
+
+  let grey = "grey";
+
+  setTimeout(() => {
+    setLight(light + 1);
+    if (light >= 3) {
+      setLight(1);
+    }
+  }, 1000);
+
   return (
-    <div className="traffiLight">
-      <div className="light red"></div>
-      <div className="light yellow"></div>
-      <div className="light green"></div>
-    </div>
+    <>
+      <div className="trafficLight">
+        <div className={`light  ${light === 1 ? "light-red" : grey}`}></div>
+        <div className={`light ${light === 2 ? "light-yellow" : grey}`}></div>
+        <div className={`light ${light === 3 ? "light-green" : grey}`}></div>
+      </div>
+    </>
   );
 }
 
