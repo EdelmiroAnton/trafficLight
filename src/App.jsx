ed4211b0d23b04e33942a2e88b31535bd60eb867
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-02-16 23:30:56 -0300

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,17 @@ import "./App.css";
 function App() {
   const [light, setLight] = useState(0);
 
-  let grey = "grey";
-
   setTimeout(() => {
     setLight(light + 1);
-    if (light >= 3) {
-      setLight(1);
-    }
+    if (light >= 3) setLight(1);
   }, 1000);
 
   return (
     <>
       <div className="trafficLight">
-        <div className={`light  ${light === 1 ? "light-red" : grey}`}></div>
-        <div className={`light ${light === 2 ? "light-yellow" : grey}`}></div>
-        <div className={`light ${light === 3 ? "light-green" : grey}`}></div>
+        <div className={`light ${light === 1 ? "light-red" : "grey"} `}></div>
+        <div className={`light ${light === 2 ? "light-yellow" : "grey"}`}></div>
+        <div className={`light ${light === 3 ? "light-green" : "grey"}`}></div>
       </div>
     </>
   );
